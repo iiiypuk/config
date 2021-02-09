3e4de9f20481d6b61085a408cb08884f039d7ed9
Alexander Popov <iiiypuk@iiiypuk.me>
2021-02-09 07:18:33 +0300

    add certbot scripts

---
+++ certbot/new.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+certbot certonly --config-dir /home/user/_certbot/config --work-dir /home/user/_certbot --logs-dir /home/user/_certbot/logs
---
+++ certbot/renew.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+certbot renew --config-dir /home/user/_certbot/config --work-dir /home/user/_certbot --logs-dir /home/user/_certbot/logs
