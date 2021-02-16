3b610d75e02f69dc7a00f582970502068e480e90
Alexander Popov <iiiypuk@iiiypuk.me>
2021-02-16 19:38:43 +0300

    add Solar2D example project

---
+++ Solar2D/SampleProject/config.lua
@@ -0,0 +1,18 @@
+application =
+{
+	content =
+	{
+		width = 1080,
+		height = 1920, 
+		scale = "letterbox",
+		fps = 60,
+		
+		--[[
+		imageSuffix =
+		{
+			    ["@2x"] = 2,
+			    ["@4x"] = 4,
+		},
+		--]]
+	},
+}
